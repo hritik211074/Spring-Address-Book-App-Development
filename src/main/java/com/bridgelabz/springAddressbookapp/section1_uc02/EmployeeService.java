08d59e284b97dd3a37f7033ddde3568be9d4d491
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-24 20:22:33 +0530

    [Hritik] adding the section1 uc02 folder into section1_uc02 branch

---
+++ src/main/java/com/bridgelabz/springAddressbookapp/section1_uc02/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.springAddressbookapp.section1_uc02;
+
+import com.bridgelabz.springaddressbookapp.model.Employee;
+import com.bridgelabz.springaddressbookapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Get all employees
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // Get employee by ID
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.orElse(null);
+    }
+
+    // Create or update employee
+    public Employee createOrUpdateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Delete employee by ID
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
