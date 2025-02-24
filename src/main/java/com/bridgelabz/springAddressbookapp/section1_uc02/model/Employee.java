150928ad61d92901f67306ee3c9122a33da6fdf7
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-24 22:37:00 +0530

    [Hritik] adding the section1 uc02 folder into section1_uc02 branch

--- src/main/java/com/bridgelabz/springAddressbookapp/section1_uc02/model/Employee.java
+++ src/main/java/com/bridgelabz/springAddressbookapp/section1_uc02/model/Employee.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.springAddressbookapp.section1_uc02.model;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
